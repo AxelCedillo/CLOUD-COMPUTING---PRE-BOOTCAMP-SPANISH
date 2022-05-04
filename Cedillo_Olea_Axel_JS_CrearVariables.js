a568c293eb3e0101a2a1faaf8150e344fa62b15b
AxelCedillo <axelcedilloolea@gmail.com>
2022-05-04 02:23:03 -0500

    Update 0.2 Variables

--- Cedillo_Olea_Axel_JS_CrearVariables.js
+++ Cedillo_Olea_Axel_JS_CrearVariables.js
@@ -1,5 +1,23 @@
-var altura  = 0;
-var edad    = 0;
-var status  = False;
+// Variables nesesarios
 
+//Variable Altura (Numero)
+var altura = 0;
 
+//  En esta varible se almacenara la altura de la persona y debe tener un valor mayor a 100 para ingresar a la atraccion.
+//  Nota: Considerando que la altura de la persona se ingresa en centimetros.
+//  100 cm = 1 m (1m la altura requerida para entrar a la atraccion).
+
+//Variable Edad (Numero)
+var edad = 0;
+
+//  En esta variable se almacenara la edad de la persona y debe tener un valor mayor a 10.
+//  Nota: Considerando que la edad se ingresa en años
+//  10 años es la edad requerida para entrar a la atraccion
+
+//Variable de estatus(Booleano)
+
+var estatus  = False;
+
+//  En esta variable se almacenara el estatus de ingreso de la persona.
+//  Con las variables antes mencionadas si cumple con los requisitos para entrar a la atraccion.
+//  Su valor pasar de "False" a "True", aprobando su acceso.
