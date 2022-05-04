4372225e728c3d62f1f0b3fc784e9b15ddaa3fa5
AxelCedillo <axelcedilloolea@gmail.com>
2022-05-04 03:56:32 -0500

    Update 0.6
    Finalización de apartado Variables

--- Variables/Prueba.js
+++ Variables/Prueba.js
@@ -1,17 +1,17 @@
 // Variables necesarias
 
 //Variable Altura (Numero)
-var altura = 110;
+var altura = 100;
 
 //Variable Edad (Numero)
-var edad = 15;
+var edad = 10;
 
 //Variable de estatus(Booleano)
 
 var estatus = false;
 
 //  Condicional para cambiar el estatus del la persona
-    if(altura > 100 && edad > 10) {
+    if(altura >= 100 || edad > 10) {
 
         //En caso de contar con los requerimientos, su estado pasa a ser "True"
         estatus = true;
@@ -24,7 +24,7 @@ var estatus = false;
     }
     //  Condicional en caso de no tener los requerimientos para entrar a la atraccion
     else if(!estatus){
-        console.log("Lo siento, chico. Tal vez el próximo año");
+        console.log("Lo siento, chico. Tal vez el próximo año.");
     }
     //  En caso de contar con error, se mostrara en consola
     else{
